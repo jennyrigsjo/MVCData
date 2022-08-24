ad4a8b638d69eee31e862622f1b1aedf750eed07
jenny <jenny.rigsjo@live.com>
2022-08-24 11:25:49 +0200

    Make list of people static and place it in separate data class.

--- MVCBasics/Models/PeopleModel.cs
+++ MVCBasics/Models/PeopleModel.cs
@@ -1,61 +1,83 @@
 ﻿using MVCBasics.ViewModels;
+using MVCBasics.Data;
 
 namespace MVCBasics.Models
 {
     public static class PeopleModel
     {
+        public static PeopleViewModel List()
+        {
+            PeopleViewModel viewModel = new()
+            {
+                List = PeopleData.List
+            };
+
+            return viewModel;
+        }
+
+
         public static PeopleViewModel Search(string keyword)
         {
             PeopleViewModel viewModel = new()
             {
-                Search = keyword
+                Search = keyword,
+                List = PeopleData.List.FindAll(person => ContainsKeyword(person, keyword))
             };
-            
-            List<Person> result = viewModel.List.FindAll(person => ContainsKeyword(person, keyword));
-            viewModel.List = result;
 
             return viewModel;
         }
+
 
         public static PeopleViewModel CreatePerson(CreatePersonViewModel person)
         {
             Person newPerson = new(person.Name, person.Phone, person.City);
 
-            PeopleViewModel viewModel = new();
+            PeopleData.List.Add(newPerson);
 
-            viewModel.List.Add(newPerson);
+            PeopleViewModel viewModel = new()
+            {
+                List = PeopleData.List
+            };
 
             return viewModel;
         }
+
 
         public static PeopleViewModel DeletePerson(string name)
         {
-            PeopleViewModel viewModel = new();
-
-            var person = viewModel.List.FirstOrDefault(person => person.Name.ToLower() == name.ToLower());
+            var person = PeopleData.List.FirstOrDefault(person => person.Name.ToLower() == name.ToLower());
 
             if (person != null)
             {
-                viewModel.List.Remove(person);
+                PeopleData.List.Remove(person);
             }
+
+            PeopleViewModel viewModel = new()
+            {
+                List = PeopleData.List
+            };
 
             return viewModel;
         }
+
 
         private static bool ContainsKeyword(Person person, string keyword)
         {
             return HasName(person, keyword) || HasPhonenumber(person, keyword) || LivesInCity(person, keyword);
         }
 
+
         private static bool HasName(Person person, string name)
         {
             return person.Name.ToLower().Contains(name.ToLower());
         }
 
+
         private static bool HasPhonenumber(Person person, string phonenumber)
         {
             return person.Phone.ToLower().Contains(phonenumber.ToLower());
         }
+
 
         private static bool LivesInCity(Person person, string city)
         {
--- MVCBasics/ViewModels/PeopleViewModel.cs
+++ MVCBasics/ViewModels/PeopleViewModel.cs
@@ -2,14 +2,7 @@
 {
     public class PeopleViewModel
     {
-        public List<Person> List { get; set; } = new List<Person>()
-        {
-            new Person("Thor Odinson", "0123 456789", "New Asgard"),
-            new Person("Erik Selvig", "1234 567890", "Stockholm"),
-            new Person("Darcy Lewis", "2345 678901", "New York"),
-            new Person("Jane Foster", "3456 789012", "New York"),
-            new Person("Loki Odinson", "4567 890123", "Valhalla"),
-        };
+        public List<Person> List { get; set; } = new List<Person>();
 
         public string Search { get; set; } = string.Empty;
     }
